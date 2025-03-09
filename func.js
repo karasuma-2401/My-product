fe11980c44d12d7f1c46836be1741bdd0c243b11
karasuma-2401 <leminhthang24012006@gmail.com>
2025-03-09 21:29:17 +0700

    calculator
    CSS color and function in JS

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
-let runningTotal =0;
-let buffer = "0";
+let runningTotal = 0;
+let buffer = '0';
 let previousOperator;
 
 const screen = document.querySelector('.screen');
@@ -11,21 +11,85 @@ function buttonClick(value){
     else{
         handleNumber(value);
     }
+    screen.innerText = buffer;
 }
 
 function handleSymbol(symbol){
     switch(symbol){
         case 'C':
             buffer = '0';
-            runningTotal =0;
+            runningTotal = 0;
             break;
         case '=':
             if (previousOperator === null){
-                return
+                return;
             }
-            flusOperation(parseInt(buffer));
+            flushOperation(parseInt(buffer));
+            previousOperator = null;
             buffer = runningTotal;
-            runningTotal =0;
+            runningTotal = 0;
+            break;
+        case '←':
+            if (buffer.length ===1){
+                buffer = '0';
+            }
+            else{
+                buffer = buffer.substring (0,buffer.length -1);
+            }
+            break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+            handleMath(symbol);
             break;
     }
 }
+
+function handleMath (symbol){
+    if (buffer === '0'){
+        return;
+    }
+    const intBuffer = parseInt (buffer);
+    
+    if (runningTotal === 0){
+        runningTotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = '0';
+}
+
+function flushOperation(intBuffer){
+    if (previousOperator === '+'){
+        runningTotal += intBuffer;
+    }
+    else if (previousOperator === '−'){
+        runningTotal -= intBuffer;
+    }
+    else if (previousOperator === '×'){
+        runningTotal *= intBuffer;
+    }
+    else if (previousOperator === '÷'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleNumber (numberString){
+    if (buffer === '0'){
+        buffer = numberString;
+    }
+    else{
+        buffer += numberString;
+    }
+}
+
+function init(){
+    document.querySelector('.calc-button').
+        addEventListener('click',function(event){
+            buttonClick(event.target.innerText);
+    })
+}
+init();
